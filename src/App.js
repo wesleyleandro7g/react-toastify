8f18421354c17bf6ea6ff355e1fcfeee8ce3d38a
wesleyleandro7g <wesleyleandro.dev@gmail.com>
2021-03-03 22:40:23 -0300

    primeiro e unico commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
+import React from 'react'
+import { ToastContainer, toast } from 'react-toastify'
+
 import logo from './logo.svg';
+
 import './App.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
+  const notify = () => toast.error("Message!", {
+    position: "bottom-left",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  })
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <button className="button" onClick={() => notify()} >Notify</button>
       </header>
+      <ToastContainer
+        position="bottom-left"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </div>
   );
 }
